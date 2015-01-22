7aab11c35f7e04951c818cf988a3cfbf9346b95d
Sebastian Forsman <sebastian.forsman@avoine.fi>
2015-01-22 12:56:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Blank site-template for AWS deployments
---
+++ install/info.php
@@ -0,0 +1,6 @@
+<?php if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "Sense Base", 
+	'summary' => "Basic PW installation that is ready to be extended", 
+	'screenshot' => ""
+	);
